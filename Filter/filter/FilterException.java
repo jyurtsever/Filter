7b2f5763e2772a245237181da418d092bb1945df
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2017-12-21 18:49:39 -0800

    first commit - no graph yet

---
+++ Filter/filter/FilterException.java
@@ -0,0 +1,17 @@
+public class FilterException extends RuntimeException {
+    /** An exception whose getMessage() value is MSG. */
+    FilterException(String msg) {
+        super(msg);
+    }
+
+    /** A utility method that returns a new exception with a message
+     *  formed from MSGFORMAT and ARGS, interpreted as for the
+     *  String.format method or the standard printf methods.
+     *
+     *  The use is thus 'throw error(...)', which tells the compiler that
+     *  execution will terminate at that point, and avoid insistance on
+     *  an explicit return in a value-returning function.)  */
+    static FilterException error(String msgFormat, Object... args) {
+        return new FilterException(String.format(msgFormat, args));
+    }
+}
---
+++ Filter/filter/Utils.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.nio.file.Files;
+
+public class Utils {
+    public static BufferedReader makeReader(String fileName) {
+
+        // The name of the file to open.
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            return new BufferedReader(
+                    new InputStreamReader(new FileInputStream(fileName), "UTF-16"));
+
+
+        } catch(FileNotFoundException ex) {
+            throw FilterException.error("Could not find file");
+
+        } catch (Exception exp) {
+            throw FilterException.error("Could not read file ");
+        }
+    }
+
+    /**
+     * Return the entire contents of FILE as a byte array.  FILE must
+     * be a normal file.  Throws IllegalArgumentException
+     * in case of problems.
+     */
+    static byte[] readContents(File file) {
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("must be a normal file");
+        }
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException excp) {
+            throw new IllegalArgumentException(excp.getMessage());
+        }
+    }
+
+    /**
+     * Return the entire contents of File of FILENAME as a byte array.
+     * FILE must be a normal file.  Throws IllegalArgumentException
+     * in case of problems.
+     */
+    static byte[] readContents(String fileName) {
+        return readContents(new File(fileName));
+    }
+}
